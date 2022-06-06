bfc2ce63981cf55c41631df042aa2de5a6d1becb
Elham Afzalo <85134558+ElhamAfzali@users.noreply.github.com>
2022-06-06 18:25:51 -0500

    Update quiz_game_project.py

--- quiz_game_project.py
+++ quiz_game_project.py
@@ -1,1 +1,28 @@
-print('hello')
+print('Welcom to my project!')
+playing = input('Do you want to play: ')
+if playing.lower() != 'yes':
+    quit()
+score = 0
+question = 0
+answer = input('What does CPU stand for? ')
+question +=1
+if answer.lower() == 'central processing unit':
+    print('Yay correct!')
+    score += 1
+else:
+    print('oops incorrect!')
+answer = input('What does GPU stand for? ')
+question +=1
+if answer.lower() == 'graphics processing unit':
+    print('Yay correct!')
+    score += 1
+else:
+    print('oops incorrect!')
+answer = input('What does RAM stand for? ')
+question +=1
+if answer.lower() == 'random access memory':
+    print('Yay correct!')
+    score += 1
+else:
+    print('oops incorrect!')
+print(f'You answered {score} questions correctly and your score is {(score/question)*100}')
